c54d4c7d4e4b532d27f6fbf53a918ea6e01082d9
Terry Brady <terrywbrady@gmail.com>
2022-02-23 12:58:53 -0800

    Update BuildFunctions.groovy

--- src/org/cdlib/mrt/build/BuildFunctions.groovy
+++ src/org/cdlib/mrt/build/BuildFunctions.groovy
@@ -42,5 +42,5 @@ def static build_library(repo, branch, mvnparams){
 }
 
 def static build_core() {
- BuildFunctions.build_library('https://github.com/CDLUC3/mrt-core2.git', "${env.BRANCH_CORE}", '-DskipTests')
+ BuildFunctions.build_library('https://github.com/CDLUC3/mrt-core2.git', env.BRANCH_CORE, '-DskipTests')
 }
