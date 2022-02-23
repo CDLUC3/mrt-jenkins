1ebf36939fb29d3708775e163cb48fe053855168
Terry Brady <terrywbrady@gmail.com>
2022-02-23 12:43:48 -0800

    Update BuildFunctions.groovy

--- src/org/cdlib/mrt/build/BuildFunctions.groovy
+++ src/org/cdlib/mrt/build/BuildFunctions.groovy
@@ -1,7 +1,46 @@
-def hello123() {
-  echo "hello 123"
+/*
+Pre-reqs
+ 
+Build Params
+- one of the following should be set
+  - branch (for dev builds)
+  - tagname (for deploy builds)
+- remove_local_m2 (to clear maven cache)
+
+Environment
+- BRANCH_XXX - branch to use for library dependencies
+  - BRANCH_CORE
+  - BRANCH_CLOUD
+- DEF_BRANCH - default branch for the service repo (TBD - determine if needed)
+- M2DIR - the m2 directory that is unique for each service being built
+*/
+
+def static init_build() {
+  script {
+    if (params.containsKey("branch")) {
+      echo 'Building branch ${params.branch}' > build.current.txt
+    } else if (params.containsKey("tagname")) {
+      echo 'Building tag ${params.tagname}' > build.current.txt
+    }
+    date >> build.current.txt
+    echo '' >> build.current.txt
+    echo 'Purge ${env.M2DIR}: ${params.remove_local_m2}'
+    if (params.remove_local_m2.toBoolean()) {
+      rm -rf ${env.M2DIR}
+    }
+  }
 }
   
-def hello456() {
-  echo "hello 456"
-}  
+def static build_library(repo, branch, mvnparams){
+  script {
+    git branch: ${branch}, url: ${repo}
+    git remote get-url origin >> ../build.current.txt
+    git symbolic-ref -q --short HEAD >> ../build.current.txt || git describe --tags --exact-match >> ../build.current.txt
+    git log --pretty=full -n 1 >> ../build.current.txt
+    mvn -Dmaven.repo.local=${env.M2DIR} -s ${MAVEN_HOME}/conf/settings.xml clean install ${mvnparams}
+  }
+}
+
+def static build_core() {
+  build_library('https://github.com/CDLUC3/mrt-core2.git', env.BRANCH_CORE, '-DskipTests')
+}
