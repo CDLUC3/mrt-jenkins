c204c9929923a2a21bbb28a508f52d2be9b8226b
Terry Brady <terrywbrady@gmail.com>
2022-03-04 15:38:47 -0800

    static/build.content.txt

--- src/org/cdlib/mrt/build/BuildFunctions.groovy
+++ src/org/cdlib/mrt/build/BuildFunctions.groovy
@@ -17,13 +17,15 @@ Environment
 
 def init_build() {
   script {
+    sh("mkdir -p static")
+    def build_txt = 'static/build.content.txt'
     if (params.containsKey("branch")) {
-      sh("echo 'Building branch ${params.branch}' > build.content.txt")
+      sh("echo 'Building branch ${params.branch}' > ${build_txt}")
     } else if (params.containsKey("tagname")) {
-      sh("echo 'Building tag ${params.tagname}' > build.content.txt")
+      sh("echo 'Building tag ${params.tagname}' > ${build_txt}")
     }
-    sh("date >> build.content.txt")
-    sh("echo '' >> build.content.txt")
+    sh("date >> ${build_txt}")
+    sh("echo '' >> ${build_txt}")
     sh("echo 'Purge ${env.M2DIR}: ${params.remove_local_m2}'")
     if (params.remove_local_m2.toBoolean()) {
       sh("rm -rf ${env.M2DIR}")
@@ -33,38 +35,41 @@ def init_build() {
   
 def build_library(repo, branch, mvnparams){
   script {
+    def build_txt = '../static/build.content.txt'
     git branch: branch, url: repo
-    sh("git remote get-url origin >> ../build.content.txt")
-    sh("git symbolic-ref -q --short HEAD >> ../build.content.txt || git describe --tags --exact-match >> ../build.content.txt")
-    sh("git log --pretty=full -n 1 >> ../build.content.txt")
+    sh("git remote get-url origin >> ${build_txt}")
+    sh("git symbolic-ref -q --short HEAD >> ${build_txt} || git describe --tags --exact-match >> ${build_txt}")
+    sh("git log --pretty=full -n 1 >> ${build_txt}")
     sh("mvn -Dmaven.repo.local=${env.M2DIR} -s ${MAVEN_HOME}/conf/settings.xml clean install ${mvnparams}")
   }
 }
 
 def build_war(repo, mvnparams) {
   script {   
+  def build_txt = '../static/build.content.txt'
     git branch: env.DEF_BRANCH, url: repo
-    sh "git remote get-url origin >> ../build.content.txt"
+    sh "git remote get-url origin >> ${build_txt}"
     if (params.containsKey("branch")) {
       checkout([
         $class: 'GitSCM',
         branches: [[name: "${params.branch}"]],
       ])
-      sh "git symbolic-ref -q --short HEAD >> ../build.content.txt || git describe --tags >> ../build.content.txt"
+      sh "git symbolic-ref -q --short HEAD >> ${build_txt} || git describe --tags >> ${build_txt}"
     } else if (params.containsKey("tagname")) {
       checkout([
         $class: 'GitSCM',
         branches: [[name: "refs/tags/${params.tagname}"]],
       ])
-      sh "git symbolic-ref -q --short HEAD >> ../build.content.txt || git describe --tags --exact-match >> ../build.content.txt"
+      sh "git symbolic-ref -q --short HEAD >> ${build_txt} || git describe --tags --exact-match >> ${build_txt}"
     }
-    sh "git log --pretty=medium -n 1 >> ../build.content.txt"
+    sh "git log --pretty=medium -n 1 >> ${build_txt}"
     sh "mvn -Dmaven.repo.local=${env.M2DIR} -s ${MAVEN_HOME}/conf/settings.xml clean install ${mvnparams}"
   }
 }
 
 def save_artifacts(path, prefix){
   script {
+    def build_txt = 'static/build.content.txt'
     def twar = "${prefix}.war"
     def tlabel = ""
     if (params.containsKey("branch")) {
@@ -73,12 +78,10 @@ def save_artifacts(path, prefix){
       tlabel = tagname
     }
     twar = "${prefix}-${tlabel}.war"
-    sh "mkdir -p WEB-INF"
-    sh "cp build.content.txt WEB-INF"
     sh "cp ${path} ${twar}"
-    sh "jar uf ${twar} WEB-INF/build.content.txt"
+    sh "jar uf ${twar} ${build_txt}"
     archiveArtifacts \
-      artifacts: "build.content.txt, ${twar}"
+      artifacts: "${build_txt}, ${twar}"
       onlyIfSuccessful: true
     if (params.containsKey("tagname")) {
       sh "mkdir -p ${JENKINS_HOME}/userContent/${prefix}"
